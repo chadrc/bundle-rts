08ba6ebec42fda83c288ad0b3c1fb24315dd1a1b
chadrc <chadrcollins91@gmail.com>
2017-03-14 20:05:39 -0400

    Converted manifest command.

--- src/bin/ManifestCommand.ts
+++ src/bin/ManifestCommand.ts
@@ -1,5 +1,35 @@
+import * as glob from "glob";
+import * as path from "path";
+import * as fs from "fs";
 import {Arguments} from "./Arguments";
+import {ensureDir, writeFile} from "./Utils";
+import {makeModuleManifestFile} from "./templates";
+
+function compileModuleManifest() {
+    let pattern = process.cwd() + "/app/modules/**/*.module.ts";
+    let moduleFiles = glob.sync(pattern);
+    let details = [];
+    for (let m of moduleFiles) {
+        let name = path.basename(m).replace(".module.ts", "");
+        let stylePath = path.dirname(m) + "/styles.scss";
+        let hasStyles = fs.existsSync(stylePath);
+        details.push({
+            name: name,
+            hasStyles: hasStyles
+        });
+    }
+    return details;
+}
+
+function writeModuleManifest() {
+    let details = compileModuleManifest();
+    let text = makeModuleManifestFile(details);
+    let dir = "/app/";
+    ensureDir(dir);
+    let basePath = process.cwd() + dir;
+    writeFile(`${basePath}module.manifest.ts`, text);
+}
 
 export function ManifestCommand(args: Arguments): void {
-
+    writeModuleManifest();
 }
