4a63335ae473f34eb6162eda87cb6829ab81d506
chadrc <chadrcollins91@gmail.com>
2017-03-15 22:13:16 -0400

    Moved all command logic into commands file. Added default in command switch.

--- src/bin/rts-seed.ts
+++ src/bin/rts-seed.ts
@@ -6,7 +6,7 @@ import * as glob from "glob";
 import {Arguments} from "./Arguments";
 import {createComponent, createModule, createProject, ensureDir, isJsIdentifier, writeFile} from "./Utils";
 import {makeIndexHTMLFile, makeIndexTSXFile, makeModuleManifestFile, makePackageJSONFile} from "./templates";
-import {manifestCommand} from "./commands";
+import {componentCommand, manifestCommand, moduleCommand, projectCommand} from "./commands";
 
 const settings = require('../package.json');
 
@@ -16,13 +16,6 @@ args.skip(2); // skip node executable and command name
 if (args.isEmpty) {
     console.log('No Arguments Given');
 } else {
-
-    let noMod = args.noModule;
-    let noComp = args.noComponent;
-    let noStyles = args.noStyles;
-    let noTypes = args.noTypes;
-    let noView = args.noView;
-
     let command = args.next();
 
     let appDir = "/app/";
@@ -32,48 +25,25 @@ if (args.isEmpty) {
     switch (command) {
         case "component":
             let componentPath = args.next();
-            let componentName = path.basename(componentPath);
-            if (isJsIdentifier(componentName)) {
-                createComponent(componentName, componentPath, noView, noTypes);
-            } else {
-                console.error("Invalid identifier: " + componentName);
-            }
+            componentCommand(componentPath, args.noView, args.noTypes);
             break;
 
         case "module":
             let moduleName = args.next();
-            if (isJsIdentifier(moduleName)) {
-                createModule(moduleName, noComp, noStyles);
-                if (!noComp) {
-                    createComponent(moduleName, `${moduleName}/${moduleName}`, noTypes, noView);
-                }
-            }
+            moduleCommand(moduleName, args.noComponent, args.noStyles, args.noTypes, args.noView);
             break;
 
         case "project":
             let appName = args.next();
-            if (!appName) {
-                console.error("No app name provided.");
-                break;
-            }
-
-            createProject(appName, basePath, settings.version);
-
-            if (!noMod) {
-                let moduleName = appName.replace(/ /g, "");
-                if (isJsIdentifier(moduleName)) {
-                    createModule(moduleName, noComp, noStyles);
-                    if (!noComp) {
-                        createComponent(moduleName, `${moduleName}/${moduleName}`, noView, noTypes);
-                    }
-                } else {
-                    console.log("Initial module could not be created. App name could not be turned into valid JavaScript identifier.");
-                }
-            }
+            projectCommand(appName, settings.version, basePath,
+                args.noModule, args.noComponent, args.noStyles, args.noView, args.noTypes);
             break;
 
         case "manifest":
             manifestCommand(basePath);
             break;
+
+        default:
+            console.error("No such command: " + command);
     }
 }
