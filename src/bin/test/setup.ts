55e61b2cba3f2be46512ff36a6499d2dd277a334
chadrc <chadrcollins91@gmail.com>
2017-03-18 09:40:38 -0400

    Changed to custom make dirs and delete dir functions to see if they'll work in travis

--- src/bin/test/setup.ts
+++ src/bin/test/setup.ts
@@ -1,22 +1,60 @@
-import * as fse from "fs-extra";
+import * as fs from "fs";
 
 let originalCwd = process.cwd();
 let workingDir = originalCwd + "/tmp/react-flares/test";
 
 export function fileExists(path: string): boolean {
-    return fse.existsSync(process.cwd() + path);
+    return fs.existsSync(process.cwd() + path);
 }
 
 export function getFileData(path: string): string {
-    return fse.readFileSync(process.cwd() + path, "utf-8");
+    return fs.readFileSync(process.cwd() + path, "utf-8");
+}
+
+function checkMakeDir(dir) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+}
+
+function ensureDir(dir) {
+    let subs = dir.split("/");
+    if (subs.length === 1) {
+        checkMakeDir(process.cwd() + "/" + subs[0]);
+    } else {
+        let current = process.cwd() + "/" + subs[0];
+        for (let i=1; i<subs.length; i++) {
+            checkMakeDir(current);
+            current += "/" + subs[i];
+        }
+    }
+}
+
+function emptyDir(dir) {
+    let children = fs.readdirSync(dir);
+    for (let child of children) {
+        let childPath = dir + "/" + child;
+        let info = fs.statSync(childPath);
+        if (info.isDirectory()) {
+            emptyDir(childPath);
+            fs.rmdirSync(childPath);
+        } else {
+            fs.unlinkSync(childPath);
+        }
+    }
+}
+
+function deleteDir(dir) {
+    emptyDir(dir);
+    fs.rmdirSync(dir);
 }
 
 beforeEach(() => {
-    fse.mkdirsSync(workingDir);
+    ensureDir("/tmp/react-flares/test/");
     process.chdir(workingDir);
 });
 
 afterEach(() => {
     process.chdir(originalCwd);
-    fse.removeSync(workingDir);
+    deleteDir(workingDir);
 });
