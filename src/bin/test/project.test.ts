5508c069837ec93cb940590557472d7a425e3cd5
chadrc <chadrcollins91@gmail.com>
2017-03-17 22:23:54 -0400

    Added test for webpack.config.js file output Undid accidentally commit change to setup.ts

--- src/bin/test/project.test.ts
+++ src/bin/test/project.test.ts
@@ -114,3 +114,73 @@ test('created tsconfig.json file should match output', () => {
 `;
     expect(data).toBe(expectedComponentText);
 });
+
+test('created webpack.config.js file should match output', () => {
+    projectCommand("MyProject", "1.0", false, false, false, false, false);
+
+    let data = getFileData(webpackFilePath);
+
+    let expectedComponentText = `\
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const glob = require("glob");
+
+let entries = {
+    app: "./app/index.tsx"
+};
+
+let locations = glob.sync("./app/modules/*/*.module.ts");
+for (let g of locations) {
+    let name = g.replace("./app/modules/", "").replace(".module.tsx").split("/")[0];
+    process.stdout.write("module: " + name + " @ " + g);
+    entries[name] = g;
+}
+
+module.exports = {
+    entry: entries,
+
+    output: {
+        filename: "js/[name].bundle.js",
+        path: __dirname + "/dist"
+    },
+
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\\.tsx?$/,
+                use: "awesome-typescript-loader"
+            },
+            {
+                test: /\\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader!sass-loader",
+                })
+            },
+            {
+                test: /\\.js$/,
+                enforce: "pre",
+                use: "source-map-loader"
+            }
+        ]
+    },
+
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+
+    plugins: [
+        new CheckerPlugin(),
+        new ExtractTextPlugin('styles/[name].bundle.css')
+    ]
+};
+`;
+    expect(data).toBe(expectedComponentText);
+});
