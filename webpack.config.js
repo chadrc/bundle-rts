7dad1443810630b4cde184584225487272d87160
chadrc <chadrcollins91@gmail.com>
2017-03-25 16:02:24 -0400

    Added webpack to build library

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+
+const libName = "ReactFlares";
+
+module.exports = {
+    entry: __dirname + "/src/lib/react-flares.ts",
+    devtool: "source-map",
+    output: {
+        path: __dirname + "/dist",
+        filename: "react-flares.js",
+        library: libName,
+        libraryTarget: "umd",
+        umdNamedDefine: true
+    },
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "awesome-typescript-loader?{configFileName: './src/lib/tsconfig.json'}"
+            },
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                use: "source-map-loader"
+            }
+        ]
+    },
+
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
