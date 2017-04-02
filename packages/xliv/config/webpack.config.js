99907c1fa6ff6237800e1e12b275cc15a52b1bbd
chadrc <chadrcollins91@gmail.com>
2017-04-01 21:52:03 -0400

    Cleaned up webpack.config.js by extracting some functionality into a util module

--- packages/xliv/config/webpack.config.js
+++ packages/xliv/config/webpack.config.js
@@ -1,33 +1,16 @@
 const { CheckerPlugin } = require('awesome-typescript-loader');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const xliv = require("xliv/utils");
 const webpack = require("webpack");
 const path = require("path");
-const glob = require("glob");
-const fs = require("fs");
 
 console.log("Building with webpack.config.js from " + __filename);
 
-let tsConfigPath = __dirname + "/tsconfig.json";
-try {
-    let localConfigPath = process.cwd() + "/tsconfig.json";
-    fs.readFileSync(localConfigPath);
-    tsConfigPath = localConfigPath
-} catch (e) {}
-
-let entries = {
-    app: "./app/index.tsx",
-    vendor: ["react", "react-dom", "react-flares"]
-};
-
-let locations = glob.sync("./app/modules/*/*.module.ts");
-for (let g of locations) {
-    let name = g.replace("./app/modules/", "").replace(".module.tsx").split("/")[0];
-    process.stdout.write("module: " + name + " @ " + g);
-    entries[name] = g;
-}
-
 module.exports = {
-    entry: entries,
+    entry: xliv.addModuleEntries({
+        app: "./app/index.tsx",
+        vendor: ["react", "react-dom", "react-flares"]
+    }),
 
     output: {
         filename: "js/[name].bundle.js",
@@ -47,7 +30,7 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: "awesome-typescript-loader?configFileName=" + tsConfigPath
+                use: "awesome-typescript-loader?configFileName=" + xliv.getTsConfig(__dirname)
             },
             {
                 test: /\.scss$/,
---
+++ packages/xliv/utils/index.ts
@@ -0,0 +1,22 @@
+import * as fs from "fs";
+import * as glob from "glob";
+
+export function getTsConfig(baseDir: string) {
+    let tsConfigPath = baseDir + "/tsconfig.json";
+    try {
+        let localConfigPath = process.cwd() + "/tsconfig.json";
+        fs.readFileSync(localConfigPath);
+        tsConfigPath = localConfigPath
+    } catch (e) {}
+    return tsConfigPath
+}
+
+export function addModuleEntries(baseEntries: {[name: string]: string | string[]}) {
+    let locations = glob.sync("./app/modules/*/*.module.ts");
+    for (let g of locations) {
+        let name = g.replace("./app/modules/", "").replace(".module.tsx", "").split("/")[0];
+        process.stdout.write("module: " + name + " @ " + g);
+        baseEntries[name] = g;
+    }
+    return baseEntries;
+}
