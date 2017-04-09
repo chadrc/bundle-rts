28964dab398325eed503bbaa073053e5efc21de8
chadrc <chadrcollins91@gmail.com>
2017-04-08 22:10:46 -0400

    ComponentFlarePlugin now generates component files

--- packages/xliv/utils/ComponentFlarePlugin.js
+++ packages/xliv/utils/ComponentFlarePlugin.js
@@ -1,38 +1,65 @@
 
-exports = module.exports = class ComponentFlarePlugin {
+exports = module.exports = class FlareComponentResolverPlugin {
     apply(compiler) {
         let componentRegex = /.(module|component)(.tsx?)?$/;
         let relativeImportRegex = /\.\.?\//;
-        let resolve = (result) => {
-            // Not a request for a component/module
-            // And issued by a file that user created
-            // Or is a relative import
-            if (!componentRegex.test(result.request)
-                || !result.contextInfo.issuer || result.contextInfo.issuer === "") {
-                return result.request;
+        let resolve = (result, fs, cb) => {
+            try {
+                // Not a request for a component/module
+                // And issued by a file that user created
+                // Or is a relative import
+                if (!componentRegex.test(result.request)
+                    || !result.contextInfo.issuer || result.contextInfo.issuer === "") {
+                    cb(result.request);
+                    return;
+                }
+                if (relativeImportRegex.test(result.request)) {
+                    cb(result.request);
+                    return;
+                }
+                let moduleName = "";
+                let pathParts =result.request.split(path.sep);
+                let modIndex = pathParts.indexOf("modules") + 1;
+                if (modIndex !== -1 && modIndex < pathParts.length) {
+                    moduleName = pathParts[modIndex];
+                }
+
+                let noComp = result.request.replace(/\.(module|component)/, "");
+                let componentName = path.basename(noComp, path.extname(noComp));
+
+                let newRequest = `gen/flares/${componentName}.flare.ts`;
+                fs.mkdirp("app/gen/flares", () => {
+                    fs.writeFile(`app/gen/flares/${componentName}.flare.ts`,
+                        makeComponentFlareFile(componentName, moduleName, false, false),
+                        {},
+                        () => {
+                            cb(newRequest);
+                        });
+                });
+            } catch (e) {
+                console.error(e);
+                cb(result.request, e);
             }
-            console.log("before resolve: " + JSON.stringify({ issuer: result.issuer, request: result.request, context: result.context}, null, 2));
-            if (relativeImportRegex.test(result.request)) {
-                console.log("relative - ignoring");
-                return result.request;
-            }
-            return result.request.replace(/^modules/, "flares").replace(/(module|component)$/, "flare");
         };
 
         compiler.plugin("normal-module-factory", (nmf) => {
             nmf.plugin("before-resolve", (result, callback) => {
                 if (!result) return callback();
-                result.request = resolve(result);
-                return callback(null, result);
-            })
+                resolve(result, compiler.outputFileSystem, (newRequest, e) => {
+                    result.request = newRequest;
+                    callback(e, result);
+                });
+            });
         });
 
         compiler.plugin("context-module-factory", (cmf) => {
             cmf.plugin("before-resolve", (result, callback) => {
                 if (!result) return callback();
-                result.request = resolve(result);
-                return callback(null, result);
-            })
+                resolve(result, compiler.outputFileSystem, (newRequest, e) => {
+                    result.request = newRequest;
+                    callback(e, result);
+                });
+            });
         });
     }
 };
